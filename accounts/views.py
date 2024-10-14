f64b1af48e26ce12d2a81ba14ff9602149f37201
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-10-14 14:04:56 +0200

    add signup and activate code

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
 
-# Create your views here.
+from .forms import SignForm,ActiveForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+def signup(request):
+   
+    '''
+    - create new user
+    - stop active this user
+    - send email to this user
+    - redirect activate html
+
+    '''
+    if request.method=='POST':
+        form=SignForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.is_active=False
+
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+
+            form.save()  # trigger create new profile
+            # send email to this user
+            profile=Profile.objects.get(user__username=username)
+            send_mail(
+            "Activate Code",
+            f"welcome mr  {username} \n pls use this code {profile.code}",
+            "r_mido99@yahoo.com",
+            [email],
+            fail_silently=False,
+                        )
+            return redirect(f'/accounts/{username}/activate')
+
+
+    else:
+        form=SignForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+def activate_code(request,username):
+   
+    '''
+    - check this code 
+    - active this user
+    - redirect login
+    '''
+    profile=Profile.objects.get(user__username=username)
+    if request.method=='POST':
+        form=ActiveForm(request.POST)
+        if form.is_valid():
+            code=form.cleaned_data['code']
+            if code==profile.code:
+                profile.code=''
+
+                user=User.objects.get(username=username)
+                user.is_active=True
+                user.is_staff=True
+
+                user.save()
+                profile.save()
+
+                return redirect('/accounts/login')
+    else:
+        form=ActiveForm()
+
+
+            
+
+    return render(request,'accounts/activate_code.html',{'form':form})
